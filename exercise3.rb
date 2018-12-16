212e03c7b61782e5669091658087191772dcecef
YudiR <yudrose13@gmail.com>
2018-12-16 12:23:16 -0500

    added age request

--- exercise3.rb
+++ exercise3.rb
@@ -1,5 +1,9 @@
 puts "whats your name?"
-user_name = get
+user_name = gets
 puts "hello, #{user_name}"
+
+puts "whats your age?"
+user_age = gets.chomp
+puts "wow! im also #{user_age} years old. \n we both have a great age."
 
 
